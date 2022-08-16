c12936f2669d477f596231d69a237b9fe6c354b6
Christopherchinia <chinia214@gmail.com>
2022-08-16 03:53:21 +0300

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet person
+    puts "Hello, #{person}!"
+end
+
+def greet_with_default person = 'programmer'
+    puts "Hello, #{person}!"
+end
+
+def add val1, val2
+    val1 + val2
+end
+
+def halve val
+    if val.class != Integer
+        return nil
+    else 
+        return val/2
+    end
+end
